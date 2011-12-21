9e84e8bbc9c57b630005933ac0f1a47b9aa564f9
benoitc <bchesneau@gmail.com>
2011-12-21 18:40:36 +0100

    refuge is our main application. It makes sense to keep it here instead having a refuge_core code.
    while I'm here remove the src/ thing.

---
+++ apps/refuge/src/refuge_app.erl
@@ -0,0 +1,16 @@
+-module(refuge_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    refuge_sup:start_link().
+
+stop(_State) ->
+    ok.
